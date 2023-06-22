cfddee049ebb1ef0150bf96598460449e49e75dc
Konstantin Kostenko <thetanqel@gmail.com>
2023-06-22 13:34:57 +0400

    add autotest for ChatService

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,56 +24,56 @@ fun main(args: Array<String>) {
 }
 
 data class Post(
-    var id: Int = 0,
-    val ownerId: Int = 0,
-    val fromId: Int = 0,
-    val createdBy: Int = 0,
-    val date: LocalDate = LocalDate.now(),
-    var text: String = "empty",
-    val replyOwnerId: Int = 0,
-    val replyPostId: Int = 0,
-    val friendsOnly: Boolean = false,
-    var comments: Comments = Comments(),
-    val copyright: String = "$ownerId",
-    val likes: Likes? = Likes(),
-    val reposts: Reposts? = Reposts(),
-    val views: Int = 0,
-    val postType: PostType = PostType.POST,
-    val postSource: PostSource? = PostSource(),
-    val attachments: Array<Attachments> = emptyArray<Attachments>(),
-    val geo: Geo? = Geo(),
-    val signerId: Int = 0,
-    //val copyHistory: Array<Post> = emptyArray<Post>(),
-    val canPin: Boolean = true,
-    val canDelete: Boolean = true,
-    val canEdit: Boolean = true,
-    val isPinned: Boolean = false,
-    val markedAsADS: Boolean = false,
-    val isFavorite: Boolean = false,
-    val postponedId: Int = 0
+        var id: Int = 0,
+        val ownerId: Int = 0,
+        val fromId: Int = 0,
+        val createdBy: Int = 0,
+        val date: LocalDate = LocalDate.now(),
+        var text: String = "empty",
+        val replyOwnerId: Int = 0,
+        val replyPostId: Int = 0,
+        val friendsOnly: Boolean = false,
+        var comments: Comments = Comments(),
+        val copyright: String = "$ownerId",
+        val likes: Likes? = Likes(),
+        val reposts: Reposts? = Reposts(),
+        val views: Int = 0,
+        val postType: PostType = PostType.POST,
+        val postSource: PostSource? = PostSource(),
+        val attachments: Array<Attachments> = emptyArray<Attachments>(),
+        val geo: Geo? = Geo(),
+        val signerId: Int = 0,
+        //val copyHistory: Array<Post> = emptyArray<Post>(),
+        val canPin: Boolean = true,
+        val canDelete: Boolean = true,
+        val canEdit: Boolean = true,
+        val isPinned: Boolean = false,
+        val markedAsADS: Boolean = false,
+        val isFavorite: Boolean = false,
+        val postponedId: Int = 0
 ) {
     enum class PostType {
         POST, COPY, REPLY, POSTPONE, SUGGEST
     }
 
     class Comments(
-        count: Int = 0,
-        canPost: Boolean = true,
-        groupsCanPostType: Boolean = true,
-        canClose: Boolean = true,
-        canOpen: Boolean = true
+            count: Int = 0,
+            canPost: Boolean = true,
+            groupsCanPostType: Boolean = true,
+            canClose: Boolean = true,
+            canOpen: Boolean = true
     )
 
     class Likes(
-        val count: Int = 0,
-        val userLikes: Boolean = true,
-        val canLike: Boolean = true,
-        val canPublish: Boolean = true
+            val count: Int = 0,
+            val userLikes: Boolean = true,
+            val canLike: Boolean = true,
+            val canPublish: Boolean = true
     )
 
     class Reposts(
-        val count: Int = 0,
-        val userReposted: Boolean = false
+            val count: Int = 0,
+            val userReposted: Boolean = false
     )
 
     class PostSource {}
@@ -81,12 +81,12 @@ data class Post(
     class Geo {}
 }
 
-interface Identifiable{
+interface Identifiable {
     var id: Int
 }
 
 open abstract class Attachments(
-    open val type: String
+        open val type: String
 )
 
 data class AttachmentsPhoto(override val type: String = "photo", val photo: Photo) : Attachments(type)
@@ -96,114 +96,114 @@ data class AttachmentsDoc(override val type: String = "doc", val doc: Doc) : Att
 data class AttachmentsNote(override val type: String = "note", val note: Note) : Attachments(type)
 
 class Photo(
-    val id: Int,
-    val albumId: Int,
-    val ownerId: Int,
-    val userId: Int,
-    val text: String,
-    val date: LocalDate,
-    val sizes: Array<Sizes> = emptyArray<Sizes>(),
-    val widthOriginal: Int,
-    val heightOriginal: Int
+        val id: Int,
+        val albumId: Int,
+        val ownerId: Int,
+        val userId: Int,
+        val text: String,
+        val date: LocalDate,
+        val sizes: Array<Sizes> = emptyArray<Sizes>(),
+        val widthOriginal: Int,
+        val heightOriginal: Int
 ) {
     class Sizes(
-        val type: String,
-        val url: URL,
-        val width: Int,
-        val height: Int
+            val type: String,
+            val url: URL,
+            val width: Int,
+            val height: Int
     ) {}
 }
 
 class Video(
-    val id: Int,
-    val ownerId: Int,
-    val title: String,
-    val description: String,
-    val duration: Int,
-    val photo320: URL?,
-    val firstFrame320: URL?,
-    val date: LocalDate,
-    val addingDate: LocalDate,
-    val views: Int,
-    val comments: Int,
-    val player: URL,
-    val platform: String,
-    val canEdit: Boolean = true,
-    val canAdd: Boolean = true,
-    val isPrivate: Boolean = false,
-    val accessKey: String,
-    val processing: Boolean = false,
-    val live: Boolean = false,
-    val upcoming: Boolean = false,
-    val isFavorite: Boolean = false
+        val id: Int,
+        val ownerId: Int,
+        val title: String,
+        val description: String,
+        val duration: Int,
+        val photo320: URL?,
+        val firstFrame320: URL?,
+        val date: LocalDate,
+        val addingDate: LocalDate,
+        val views: Int,
+        val comments: Int,
+        val player: URL,
+        val platform: String,
+        val canEdit: Boolean = true,
+        val canAdd: Boolean = true,
+        val isPrivate: Boolean = false,
+        val accessKey: String,
+        val processing: Boolean = false,
+        val live: Boolean = false,
+        val upcoming: Boolean = false,
+        val isFavorite: Boolean = false
 
 ) {}
 
 class Audio(
-    val id: Int,
-    val ownerId: Int,
-    val artist: String,
-    val title: String,
-    val duration: Int,
-    val url: URL,
-    val lyricsId: Int,
-    val albumId: Int,
-    val genreId: Int,
-    val date: LocalDate,
-    val noSearch: Boolean = false,
-    val isHQ: Boolean = true
+        val id: Int,
+        val ownerId: Int,
+        val artist: String,
+        val title: String,
+        val duration: Int,
+        val url: URL,
+        val lyricsId: Int,
+        val albumId: Int,
+        val genreId: Int,
+        val date: LocalDate,
+        val noSearch: Boolean = false,
+        val isHQ: Boolean = true
 ) {}
 
 class Doc(
-    val id: Int,
-    val ownerId: Int,
-    val title: String,
-    val size: Int,
-    val url: URL,
-    val date: LocalDate,
-    val type: Int = 1
+        val id: Int,
+        val ownerId: Int,
+        val title: String,
+        val size: Int,
+        val url: URL,
+        val date: LocalDate,
+        val type: Int = 1
 ) {}
 
 data class Note(
-    override var id: Int = 0,
-    val ownerId: Int = 0,
-    val title: String = "empty tittle",
-    var text: String = "empty text",
-    val date: LocalDate = LocalDate.now(),
-    val comments: Int = 0,
-    val readComments: Int = 0,
-    //val viewUrl: URL = URL("localhost"),
-    val privacyView: Boolean = true,
-    val canComment: Boolean = true,
-    val textWiki: String = "_"
-) : Identifiable{}
+        override var id: Int = 0,
+        val ownerId: Int = 0,
+        val title: String = "empty tittle",
+        var text: String = "empty text",
+        val date: LocalDate = LocalDate.now(),
+        val comments: Int = 0,
+        val readComments: Int = 0,
+        //val viewUrl: URL = URL("localhost"),
+        val privacyView: Boolean = true,
+        val canComment: Boolean = true,
+        val textWiki: String = "_"
+) : Identifiable {}
 
 data class Comment(
-    override var id: Int = 0,
-    var fromId: Int = 0,
-    val date: LocalDate = LocalDate.now(),
-    var text: String = "empty comment",
+        override var id: Int = 0,
+        var fromId: Int = 0,
+        val date: LocalDate = LocalDate.now(),
+        var text: String = "empty comment",
 //    val donut: Donut = Donut(),
-    val replyToUser: Int = 0,
-    val replyToComment: Int = 0,
-    val attachments: Array<Attachments> = emptyArray<Attachments>(),
-    val parentsStack: Array<Int> = emptyArray<Int>(),
-    val threadComments: ThreadComments = ThreadComments(),
-    var isDeleted: Boolean = false
+        val replyToUser: Int = 0,
+        val replyToComment: Int = 0,
+        val attachments: Array<Attachments> = emptyArray<Attachments>(),
+        val parentsStack: Array<Int> = emptyArray<Int>(),
+        val threadComments: ThreadComments = ThreadComments(),
+        var isDeleted: Boolean = false
 ) : Identifiable {
     class ThreadComments(
-        val count: Int = 0,
-        val items: Array<Comment> = emptyArray<Comment>(),
-        val canPost: Boolean = true,
-        val showReplyButton: Boolean = true,
-        val groupsCanPost: Boolean = true
+            val count: Int = 0,
+            val items: Array<Comment> = emptyArray<Comment>(),
+            val canPost: Boolean = true,
+            val showReplyButton: Boolean = true,
+            val groupsCanPost: Boolean = true
     )
 }
 
 data class ReportComment(
-    var ownerId: Int = 0,
-    val commentId: Int = 0,
-    var reason: ReasonReport?
+        var ownerId: Int = 0,
+        val commentId: Int = 0,
+        var reason: ReasonReport?
 ) {
     enum class ReasonReport(value: Int) {
         SPAM(0),
@@ -215,61 +215,78 @@ data class ReportComment(
         SUICAID(8)
     }
 }
+
 data class Chat(
-    override var id: Int,
-    var lastUniqId:Int = 0,
-    val idOwner: Int,
-    var messages: MutableList<DirectMessages> = mutableListOf<DirectMessages>()
-) : Identifiable  { data class DirectMessages(
-        override var id: Int,
-        val idSender: Int = 0,
-        val idRecipient: Int = 0,
-        var text: String = "",
-        var isRead: Boolean = false
-    ): Identifiable
+        override var id: Int = 0,
+        var lastUniqId: Int = 0,
+        val idOwner: Int = 0,
+        var messages: MutableList<DirectMessages> = mutableListOf<DirectMessages>()
+) : Identifiable {
+    data class DirectMessages(
+            override var id: Int = 0,
+            val idSender: Int = 0,
+            val idRecipient: Int = 0,
+            var text: String = "",
+            var isRead: Boolean = false
+    ) : Identifiable
 }
 
-object ChatService{
+object ChatService {
     var chats = mutableListOf<Chat>()
     var uniqIdChat = 0
 
     //Видеть, сколько чатов не прочитано (например, service.getUnreadChatsCount).
     // В каждом из таких чатов есть хотя бы одно непрочитанное сообщение.
-    fun getUnreadChatsCount(idOwner: Int){
-        chats.filter {it.idOwner == idOwner}.filter{ (it.messages.filter { !it.isRead }).isNotEmpty() }
+    fun getUnreadChatsCount(idUser: Int): Int {
+
+        return chats.filter { it ->
+            (it.messages.filter { !it.isRead }).isNotEmpty()
+        }.filter { it ->
+            (it.messages.filter { it.idSender==idUser }).isNotEmpty()
+        }.size
+
+
+
     }
 
     //Получить список чатов
-    fun getChats(idOwner: Int) : MutableList<Chat>{
-       return chats.filter {it.idOwner == idOwner}.toMutableList()
+    fun getChats(idOwner: Int): MutableList<Chat> {
+        return chats.filter { it.idOwner == idOwner }.toMutableList()
+    }
+
+    //Типа моя функция расширения, помечает диапазон сообщений как прочитанный
+    private fun MutableList<Chat.DirectMessages>.markRead(idStart: Int, idEnd: Int) {
+        this.filter { it.id in (idStart + 1)..idEnd }.forEach { it.isRead = true }
     }
 
     //Получить список сообщений из чата
-    fun getMessages(idChat: Int, idLastReadMessages: Int, quantityMessage: Int) : MutableList<Chat.DirectMessages>{
-        val messages = chats.filter { it.id == idChat }[0].messages.filter { it.id >= idLastReadMessages}.toMutableList()
-        val messagesToRead = messages.filter {messages.indexOf(it) <= quantityMessage}.toMutableList()
-        chats.filter { it.id == idChat }[0].messages.filter { it.id >= idLastReadMessages && it.id <= messagesToRead.last().id }.forEach{ it.isRead = true }
+    fun getMessages(idChat: Int, idLastReadMessages: Int, quantityMessage: Int): MutableList<Chat.DirectMessages> {
+        val messages = chats.filter { it.id == idChat }[0].messages.filter { it.id > idLastReadMessages }.toMutableList()
+        val messagesToRead = messages.filter { messages.indexOf(it) <= quantityMessage - 1 }.toMutableList()
+        chats.filter { it.id == idChat }[0].messages.markRead(idLastReadMessages, messagesToRead.last().id)
         return messagesToRead
     }
 
-    fun createMessage(idUser: Int, idTo: Int, idChat: Int, text:String): Chat.DirectMessages{
-        chats[getIndexById(idChat,chats)].lastUniqId++
-        val mess = Chat.DirectMessages(chats[getIndexById(idChat,chats)].lastUniqId,idUser, idTo, text)
-        chats[getIndexById(idChat,chats)].messages.add(mess)
-        return chats[getIndexById(idChat,chats)].messages.last()
-    }
-    fun editMessage(idChat: Int, idMessage: Int, text:String){
-        val messagesForEdit = chats[getIndexById(idChat,chats)].messages
-        messagesForEdit[getIndexById(idMessage,messagesForEdit)].text = text
-        chats[getIndexById(idChat,chats)].messages = messagesForEdit
-    }
-    fun deleteMessage(idChat: Int, idMessage: Int){
-        val messagesForEdit = chats[getIndexById(idChat,chats)].messages
-        messagesForEdit.remove(messagesForEdit[getIndexById(idMessage, messagesForEdit)])
-        chats[getIndexById(idChat,chats)].messages = messagesForEdit
+    fun createMessage(idUser: Int, idTo: Int, idChat: Int, text: String): Chat.DirectMessages {
+        chats[getIndexById(idChat, chats)].lastUniqId++
+        val mess = Chat.DirectMessages(chats[getIndexById(idChat, chats)].lastUniqId, idUser, idTo, text)
+        chats[getIndexById(idChat, chats)].messages.add(mess)
+        return chats[getIndexById(idChat, chats)].messages.last()
     }
 
-    fun createChat(idUser: Int, idRecipient: Int, firstMessage: String): Chat{
+    fun editMessage(idChat: Int, idMessage: Int, text: String) {
+        val messagesForEdit = chats[getIndexById(idChat, chats)].messages
+        messagesForEdit[getIndexById(idMessage, messagesForEdit)].text = text
+        chats[getIndexById(idChat, chats)].messages = messagesForEdit
+    }
+
+    fun deleteMessage(idChat: Int, idMessage: Int) {
+        val messagesForEdit = chats[getIndexById(idChat, chats)].messages
+        messagesForEdit.remove(messagesForEdit[getIndexById(idMessage, messagesForEdit)])
+        chats[getIndexById(idChat, chats)].messages = messagesForEdit
+    }
+
+    fun createChat(idUser: Int, idRecipient: Int, firstMessage: String): Chat {
         var messages = mutableListOf<Chat.DirectMessages>()
         uniqIdChat++
         chats.add(Chat(uniqIdChat, idOwner = idUser, messages = messages))
@@ -277,8 +294,14 @@ object ChatService{
         return chats.last()
     }
 
-    fun deleteChat(idChat: Int):Boolean{
+    fun deleteChat(idChat: Int): Boolean {
         return chats.remove(chats[getIndexById(idChat, chats)])
+    }
+
+    fun clear() {
+        chats.removeAll(chats)
+        uniqIdChat = 0
+        // также здесь нужно сбросить счетчик для id постов, если он у вас используется
     }
 }
 
@@ -361,7 +384,7 @@ object NoteService {
 
     fun createComment(comment: Comment, fromIdNote: Int): Comment {//Добавляет новый комментарий к заметке.
         uniqIdComment++
-         val createComment: Comment = comment.copy()
+        val createComment: Comment = comment.copy()
         createComment.id = uniqIdComment
         createComment.fromId = fromIdNote
         createComment.text = "Комментарий #$uniqId"
@@ -374,14 +397,15 @@ object NoteService {
 
         val newCommentsList: MutableList<Comment> = mutableListOf<Comment>()
 
-        for (comment in noteComments){
-            if(comment.fromId != idNote){
+        for (comment in noteComments) {
+            if (comment.fromId != idNote) {
                 newCommentsList.add(comment)
             }
         }
         noteComments = newCommentsList
         return notes.remove(notes[indexNote])
     }
+
     fun deleteComment(idComment: Int) {//Удаляет комментарий к заметке, а именно помечает комментарий как удаленный
         noteComments[getIndexById(idComment, noteComments)].isDeleted = true
     }
@@ -409,7 +433,8 @@ object NoteService {
     }
 
     fun getById(idNote: Int): Note {//Возвращает заметку по её id.
-        return (notes.getOrNull(getIndexById(idNote, notes)) ?: throw SomethingWrongException("Такой записи нет")).copy()
+        return (notes.getOrNull(getIndexById(idNote, notes))
+                ?: throw SomethingWrongException("Такой записи нет")).copy()
     }
 
     fun getComments(): MutableList<Comment> {//Возвращает список комментариев к заметке.(не удаленных)
@@ -425,7 +450,7 @@ object NoteService {
     fun restoreComment(idComment: Int) {// Восстанавливает удаленный комментарий
         val indexComment = getIndexById(idComment, noteComments)
         val restoreComment =
-            (noteComments.getOrNull(indexComment) ?: throw SomethingWrongException("Такого комментария нет")).copy()
+                (noteComments.getOrNull(indexComment) ?: throw SomethingWrongException("Такого комментария нет")).copy()
         if (restoreComment.isDeleted) {
             restoreComment.isDeleted = false
             noteComments[indexComment] = restoreComment
@@ -468,9 +493,10 @@ object NoteService {
     //для работы этого метода добавлен интерфейс Identifiable, чтобы не прописывать when
 
 }
-fun <T:Identifiable> getIndexById(id: Int, list: MutableList<T>): Int { //возврат индекса записи в коллекции
+
+fun <T : Identifiable> getIndexById(id: Int, list: MutableList<T>): Int { //возврат индекса записи в коллекции
     var index: Int = 0
-    var isExist:Boolean = false
+    var isExist: Boolean = false
     for (element in list) {
         if (element.id == id) {
             index = list.indexOf(element)
---
+++ src/test/kotlin/ChatServiceTest.kt
@@ -0,0 +1,140 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import javax.lang.model.util.Elements.Origin
+
+class ChatServiceTest {
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
+
+    @Test
+    fun getUnreadChatsCount() {
+        //arrange
+        //ищем чаты где есть не прочитанные сообщения от 1 пользователя
+        val chat1:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я сообщение 2 от 1 в чате 1")
+        val mess1Chat1 = ChatService.createMessage(2, 1, 1, "2: Я прочитал сообщение №1 в чате 1")
+        val mess2Chat1 = ChatService.createMessage(1, 2, 1, "1: Я прочитал сообщение №1 от 2 в чате 1")
+        ChatService.getMessages(1, 0,3) //все прочитаны
+        val chat2:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я сообщение 2 от 1 в чате 2")
+        val mess1Chat2 = ChatService.createMessage(2, 1, 2, "2: Я прочитал сообщение №1 в чате 2")
+        val mess2Chat2 = ChatService.createMessage(1, 2, 2, "1: Я не прочитал сообщение №1 от 2 в чате 2")
+        ChatService.getMessages(2, 0,2) // 1 не прочитано (1 от 1 пользв)
+        val chat3:Chat = ChatService.createChat(idUser = 2, idRecipient = 1, firstMessage = "Ты тут?, это сообщение 1 от 2 в чате 3")
+        val mess1Chat3 = ChatService.createMessage(1, 2, 3, "2: Я не прочитал сообщение №1 в чате 3")
+        val mess2Chat3 = ChatService.createMessage(2, 1, 3, "1: Я не прочитал сообщение №1 от 2 в чате 3")
+        ChatService.getMessages(3, 1,2) // 2 не прочитано вообще (1 от 1 пользв)
+        val chat4:Chat = ChatService.createChat(idUser = 2, idRecipient = 1, firstMessage = "Ты тут?, это сообщение 1 от 2 в чате 4")
+        val mess1Chat4 = ChatService.createMessage(1, 2, 4, "2: Я не прочитал сообщение №1 в чате 4")
+        val mess2Chat4 = ChatService.createMessage(2, 1, 4, "1: Я не прочитал сообщение №1 от 2 в чате 4")
+        //вообще все не прочитаны (1 от 1 пользв)
+
+        //act
+        val count: Int = ChatService.getUnreadChatsCount(1)
+        //assert
+        assertEquals(3, count)
+    }
+
+    @Test
+    fun getChats() {
+        //arrange
+        val chat1:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я 1")
+        val chat2:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я 1")
+        val chat3:Chat = ChatService.createChat(idUser = 2, idRecipient = 1, firstMessage = "Ты тут?")
+        //act
+        val chatsOwner1 = ChatService.getChats(1)
+        //assert
+        assertEquals(2, chatsOwner1.size)
+        assertEquals(1, chatsOwner1[0].idOwner)
+        assertEquals(1, chatsOwner1[1].idOwner)
+    }
+
+    @Test
+    fun getMessages() {
+        //arrange
+        val chat:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я 1")
+        val mess2 = ChatService.createMessage(2, 1, 1, "Привет, а я 2")
+        val mess3 = ChatService.createMessage(1, 2, 1, "Это не прочитанное сообщение от 1")
+        val mess4 = ChatService.createMessage(1, 2, 1, "А это не прочитанное сообщение от 2")
+        val mess5 = ChatService.createMessage(1, 2, 1, "Это второе не прочитанное сообщение от 1")
+        val mess6 = ChatService.createMessage(1, 2, 1, "А это второе не прочитанное сообщение от 2")
+        chat.messages[0].isRead = true
+        chat.messages[1].isRead = true
+        //act
+        val unreadMessages = ChatService.getMessages(1, 2, 3)
+        //assert
+        assertEquals(3, unreadMessages.size)
+        assertEquals("Это не прочитанное сообщение от 1", unreadMessages[0].text)
+        assertTrue(unreadMessages[0].isRead)
+        assertEquals("А это не прочитанное сообщение от 2", unreadMessages[1].text)
+        assertTrue(unreadMessages[1].isRead)
+        assertEquals("Это второе не прочитанное сообщение от 1", unreadMessages[2].text)
+        assertTrue(unreadMessages[2].isRead)
+
+    }
+
+    @Test
+    fun createMessage() {
+        //arrange
+        val chat:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я 1")
+        val etalonMessage: Chat.DirectMessages = Chat.DirectMessages(2, 2, 1, "Привет, а я 2", false)
+        //act
+        val mess = ChatService.createMessage(2, 1, 1, "Привет, а я 2")
+        //assert
+        assertEquals(etalonMessage, mess)
+    }
+
+    @Test
+    fun editMessage() {
+        //arrange
+        val chat:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я 1")
+        val mess = ChatService.createMessage(2, 1, 1, "Привет, а я 2")
+        //act
+        ChatService.editMessage(1, 2, "Здрав буде боярин, последующий за тобой я.")
+        //
+        assertEquals("Здрав буде боярин, последующий за тобой я.", chat.messages[1].text)
+
+    }
+
+    @Test
+    fun deleteMessage() {
+        //arrange
+        val chat:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я 1")
+        val mess = ChatService.createMessage(2, 1, 1, "Привет, а я 2")
+        //act
+        ChatService.deleteMessage(1, 2)
+        //assert
+        assertEquals(1, chat.messages.size)
+        assertEquals(1, chat.messages[0].id)
+    }
+
+    @Test
+    fun createChat() {
+        //arrange
+        val messages: MutableList<Chat.DirectMessages>  = mutableListOf<Chat.DirectMessages>()
+        val messageFirst: Chat.DirectMessages = Chat.DirectMessages(1, 1, 2, "Привет", false)
+        messages.add(messageFirst)
+        val chat1:Chat = Chat(id = 1, lastUniqId = 1, idOwner = 1, messages = messages)
+        //act
+        val chat2:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет")
+        //assert
+        assertEquals(chat1, chat2)
+    }
+
+    @Test
+    fun deleteChat() {
+        //arrange
+        val chat1:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я 1")
+        val chat2:Chat = ChatService.createChat(idUser = 1, idRecipient = 2, firstMessage = "Привет, я 1")
+        val chat3:Chat = ChatService.createChat(idUser = 2, idRecipient = 1, firstMessage = "Ты тут?")
+        //act
+        ChatService.deleteChat(2)
+        //assert
+        assertEquals(2, ChatService.chats.size)
+        assertEquals(1, ChatService.chats[0].id)
+        assertEquals(3, ChatService.chats[1].id)
+
+    }
+}
